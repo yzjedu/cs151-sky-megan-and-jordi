a83917036eaf6547ccbfd989b0cc1c1420a6a623
mwijdoogen <mwijdoogen@loyola.edu>
2024-09-26 13:32:25 -0400

    Commit algorithm and py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-# Enter Code here
-# Read the README - under files on left
-# Delete these three lines before submitting
+# Programmers: Megan, Jordi
+# Course: Cs151, Professor Zee
+# Due Date: 10/3/2024
+# Lab Assignment: 3
+# Problem statement: Make a program to calculate the distance traveled based on speed and /
+# determine how many points the skier would receive if they went that distance.
+
+# Data In: Code needs input the distance the skier wants to jump  in meter, the height of the jump in meter, the points per meter, /
+# the par in meter, the skiers speed at the end of the jump,
+# Data Out: How many points the skier will receive based on their distance traveled
+
+# User should be prompted to enter five inputs: the height of the jump in meters, the skiers speed,
+
+points_per_meter = 2
+print('What is the type of ski jump?')
+height_jump = float(input())
+print('What is the skiers speed at the end of the jump in km/h?')
+skiers_speed = float(input())
+
